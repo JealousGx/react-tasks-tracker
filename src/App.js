7c40853def247d263d17df24aa98a12f887c7fa2
JealousGx <59867217+JealousGx@users.noreply.github.com>
2021-08-04 19:21:12 +0500

    Modified the package json and added the server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react'
-import { Route } from 'react-router-dom';
-import About from './components/About';
+import { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import About from "./components/About";
 
-import AddTask from './components/AddTask';
-import Footer from './components/Footer';
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -16,33 +15,40 @@ function App() {
     const getTasks = async () => {
       const tasksFromServer = await fetchTasks();
       setTasks(tasksFromServer);
-    }
+    };
     getTasks();
-  }, [])
+  }, []);
 
   //fetch tasks
   const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks');
+    const res = await fetch(
+      "https://my-json-server.typicode.com/jealousgx/tasks-tracker-dbjson/tasks"
+    );
     const data = await res.json();
     return data;
-  }
+  };
 
   //fetch task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(
+      `https://my-json-server.typicode.com/jealousgx/tasks-tracker-dbjson/tasks/${id}`
+    );
     const data = await res.json();
     return data;
-  }
+  };
 
   //Add task
   const addTask = async (task) => {
-    const res = await fetch(`http://localhost:5000/tasks`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(task)
-    });
+    const res = await fetch(
+      `https://my-json-server.typicode.com/jealousgx/tasks-tracker-dbjson/tasks`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
 
     const data = await res.json();
     setTasks([...tasks, data]);
@@ -50,55 +56,69 @@ function App() {
     // const id = Math.floor(Math.random() * 10000 + 1);
     // const newTask = { id, ...task };
     // setTasks([...tasks, newTask]);
-  }
+  };
 
   //Delete task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'DELETE'
-    });
+    await fetch(
+      `https://my-json-server.typicode.com/jealousgx/tasks-tracker-dbjson/tasks/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
     setTasks(tasks.filter((task) => task.id !== id));
-  }
+  };
 
   //Toggle reminder
   const toggleReminder = async (id) => {
     const taskToToggle = await fetchTask(id);
     const updtTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(updtTask)
-    });
+    const res = await fetch(
+      `https://my-json-server.typicode.com/jealousgx/tasks-tracker-dbjson/tasks/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updtTask),
+      }
+    );
 
     const data = await res.json();
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
-  }
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
-      <Header 
-        onAdd={() => setShowAddTask(!showAddTask)} 
-        title={'Tasks Tracker'} 
-        showAdd={showAddTask}  
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        title={"Tasks Tracker"}
+        showAdd={showAddTask}
       />
-      
-      <Route path='/' exact render={(props) => (
-        <>
-          {showAddTask && <AddTask onAdd={addTask} />}
-      {
-        tasks.length > 0 ? 
-        <Tasks 
-          tasks={tasks} 
-          onDelete={deleteTask} 
-          onToggle={toggleReminder}
-        /> : 
-        'No tasks to show!'
-      }
-        </>
-      )} />
-      <Route path='/about' component={About} />
+
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? (
+              <Tasks
+                tasks={tasks}
+                onDelete={deleteTask}
+                onToggle={toggleReminder}
+              />
+            ) : (
+              "No tasks to show!"
+            )}
+          </>
+        )}
+      />
+      <Route path="/about" component={About} />
       <Footer />
     </div>
   );
